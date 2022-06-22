c7b825f8af800c27a32c8ec8e865a8d12956b59e
unknown <datdonguyen99@gmail.com>
2022-06-22 09:35:51 +0700

    test-1

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const aedes = require("aedes")();
 const httpServer = require("http").createServer();
 const ws = require("websocket-stream");
-const port = process.env.PORT || 80;
+require("dotenv").config();
+const wsPort = process.env.WSPORT || 80;
 
 ws.createServer({ server: httpServer }, aedes.handle);
 
-httpServer.listen(port, () => {
-  console.log("websocket server listening on port ", port);
+httpServer.listen(wsPort, () => {
+  console.log("websocket server listening on port ", wsPort);
 });
